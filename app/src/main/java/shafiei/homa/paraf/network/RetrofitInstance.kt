3c00cff9a4c3b23fcab27c0bf0fcbf1199f4d875
Homa Shafiei <hardware.engineer@yahoo.com>
2022-06-09 15:49:16 +0430

    feature: add cache to retrofit

--- app/src/main/java/shafiei/homa/paraf/network/RetrofitInstance.kt
+++ app/src/main/java/shafiei/homa/paraf/network/RetrofitInstance.kt
@@ -1,11 +1,13 @@
 package shafiei.homa.paraf.network
 
 import com.readystatesoftware.chuck.ChuckInterceptor
+import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import shafiei.homa.paraf.MyApplication
+import java.io.File
 
 abstract class RetrofitInstance {
 
@@ -17,17 +19,24 @@ abstract class RetrofitInstance {
             val logging = HttpLoggingInterceptor()
             logging.setLevel(HttpLoggingInterceptor.Level.BODY)
 
+            //setup cache
+            val httpCacheDirectory =
+                File(MyApplication.instance.applicationContext.cacheDir, "responses")
+            val cacheSize = 10 * 1024 * 1024L // 10 MiB
+            val cache = Cache(httpCacheDirectory, cacheSize)
+
             val client = OkHttpClient.Builder()
-                    .addInterceptor(ResponseInterceptor())
-                    .addInterceptor(ChuckInterceptor(MyApplication.instance))
-                    .addInterceptor(logging)
-                    .build()
+                .addInterceptor(ResponseInterceptor())
+                .addInterceptor(ChuckInterceptor(MyApplication.instance))
+                .addInterceptor(logging)
+                .cache(cache)
+                .build()
 
             Retrofit.Builder()
-                    .baseUrl("https://api.themoviedb.org/3/")
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .client(client)
-                    .build()
+                .baseUrl("https://api.themoviedb.org/3/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
         }
     }
 }
